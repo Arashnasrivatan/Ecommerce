ee67398baf276f252d8270be94f0a2fb9a6b529e
Arashnasrivatan <arashnasrivatan@gmail.com>
2025-03-02 21:28:06 +0330

    add Comments model

---
+++ src/models/Comment.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const repliesSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const commentSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    replies: {
+      type: [repliesSchema],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
