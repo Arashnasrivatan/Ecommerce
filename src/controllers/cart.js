43aa72652343940bab98909b2e726f3086adcd25
Arashnasrivatan <arashnasrivatan@gmail.com>
2025-03-06 22:46:22 +0330

    complete addToCart method

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -9,6 +9,7 @@ exports.getCart = async (req, res, next) => {
     const { user_id } = req.query;
     const isAdmin = req.user.role === "ADMIN";
     let user;
+
     if (isAdmin && user_id) {
       if (!isValidObjectId(user_id)) {
         return response(res, 400, "Invalid user_id");
@@ -23,12 +24,21 @@ exports.getCart = async (req, res, next) => {
     }
 
     const cart = await Cart.findOne({ user: user._id })
-      .populate("items.product", "name images")
+      .populate("items.product", "name images priceInRial")
       .select("-user");
 
     if (!cart) {
       return response(res, 404, "Cart not found");
     }
+
+    for (let item of cart.items) {
+      const product = await Product.findById(item.product._id);
+      if (product && product.priceInRial !== item.price) {
+        item.price = product.priceInRial;
+      }
+    }
+
+    await cart.save();
 
     return response(res, 200, "Cart Fetched Successfully", {
       cart,
@@ -41,7 +51,78 @@ exports.getCart = async (req, res, next) => {
 
 exports.addToCart = async (req, res, next) => {
   try {
-    // TODO
+    const { user_id } = req.query;
+    const isAdmin = req.user.role === "ADMIN";
+    let user;
+
+    if (isAdmin && user_id) {
+      if (!isValidObjectId(user_id)) {
+        return response(res, 400, "Invalid user_id");
+      }
+      user = await User.findById(user_id);
+    } else {
+      user = req.user;
+    }
+
+    if (!user) {
+      return response(res, 404, "User not found");
+    }
+
+    const { productId } = req.body;
+    const quantity = req.validatedBody.quantity;
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return response(res, 404, "Product not found");
+    }
+
+    const price = product.priceInRial;
+    const cart = await Cart.findOne({ user: user._id });
+
+    if (!cart) {
+      const newCart = await Cart.create({
+        user: user._id,
+        items: [
+          {
+            product: productId,
+            quantity,
+            price,
+          },
+        ],
+      });
+
+      return response(res, 201, "Cart created successfully", newCart);
+    }
+
+    const index = cart.items.findIndex(
+      (item) => item.product.toString() === productId.toString()
+    );
+
+    if (index !== -1) {
+      if (cart.items[index].quantity + quantity > 100) {
+        return response(res, 400, "Quantity exceeds maximum limit of 100");
+      }
+      cart.items[index].quantity = quantity + cart.items[index].quantity;
+      cart.items[index].price = price;
+    } else {
+      cart.items.push({
+        product: productId,
+        quantity,
+        price,
+      });
+    }
+
+    for (let item of cart.items) {
+      const product = await Product.findById(item.product);
+      if (product && product.priceInRial !== item.price) {
+        item.price = product.priceInRial;
+      }
+    }
+
+    await cart.save();
+
+    return response(res, 200, "Cart updated successfully", cart);
   } catch (err) {
     next(err);
   }
