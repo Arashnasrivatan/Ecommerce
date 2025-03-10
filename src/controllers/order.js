5606019b2ef843e895bff31bb0b4bf7f9347db71
Arashnasrivatan <arashnasrivatan@gmail.com>
2025-03-10 14:20:19 +0330

    complete updateOrder method

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -2,14 +2,14 @@ const response = require("./../utils/response");
 const Order = require("./../models/Order");
 const { createPaginationData } = require("./../utils/index");
 
-// not tested yet
 exports.getAllOrders = async (req, res, next) => {
   try {
     const { page = 1, limit = 10, user_id = null } = req.query;
     const user = req.user;
+    const isAdmin = user.role == "ADMIN";
 
     const filters = {
-      ...(user.role == "ADMIN" ? {} : { user: user._id }),
+      ...(isAdmin ? {} : { user: user._id }),
       ...(user_id ? { user: user_id } : {}),
     };
 
@@ -30,12 +30,7 @@ exports.getAllOrders = async (req, res, next) => {
 
     const countOrders = await Order.countDocuments(filters);
 
-    const pagination = createPaginationData(
-      page,
-      limit,
-      countOrders,
-      "Orders"
-    );
+    const pagination = createPaginationData(page, limit, countOrders, "Orders");
 
     return response(res, 200, "Orders fetched successfully", {
       pagination,
@@ -46,9 +41,34 @@ exports.getAllOrders = async (req, res, next) => {
   }
 };
 
+// not tested yet | test after complete checkout Controller&Router
 exports.updateOrder = async (req, res, next) => {
   try {
-    // TODO
+    let { postTrackingCode, status } = req.body;
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return response(res, 400, "Invalid order ID");
+    }
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return response(res, 404, "Order not found");
+    }
+
+    if (postTrackingCode && !status) {
+      status = "SHIPPED";
+    }
+
+    order.status = status || order.status;
+    order.postTrackingCode = postTrackingCode || order.postTrackingCode;
+
+    const updatedOrder = await order.save();
+
+    return response(res, 200, "Order updated successfully :))", {
+      updatedOrder,
+    });
   } catch (err) {
     next(err);
   }
