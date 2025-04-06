4c5eb86ed00d5f2361d7c2b81948279e5e45ef7b
Arashnasrivatan <arashnasrivatan@gmail.com>
2025-04-06 13:15:09 +0330

    add change-password route doc

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,6 @@ const response = require("./../utils/response");
 const User = require("./../models/User");
 const bcrypt = require("bcrypt");
 const { isValidObjectId } = require("mongoose");
-const { getLocationDetails, isLocationInIran } = require("./../utils/index");
 
 exports.editProfile = async (req, res, next) => {
   try {
